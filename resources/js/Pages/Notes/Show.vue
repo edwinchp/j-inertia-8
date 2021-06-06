d7c13f51faf2e245b8a11c48b4dda48888753df7
Edwin Chi Pineiro <chipineiro@gmail.com>
2021-06-05 19:28:18 -0500

    notes.show implemented

--- resources/js/Pages/Notes/Show.vue
+++ resources/js/Pages/Notes/Show.vue
@@ -11,31 +11,18 @@
         <div class="md:grid md:grid-cols-3 md:gap-6">
           <div class="md:col-span-1">
             <div class="px-4 sm:px0">
-              <h3 class="text-lg text-gray-900">Listado de notas</h3>
+              <h3 class="text-lg text-gray-900">Detalle de una nota</h3>
               <p class="text-sm text-gray-600">
-                Toma el registro correcto y ejecuta cualquier función
+                {{ note.excerpt }}
               </p>
             </div>
           </div>
+
           <div class="md:col-span-2 mt-5 md:mt-0">
             <div class="shadow bg-white md:rounded-md p-4">
-              <table>
-                <tr v-for="(note, index) in notes" :key="index">
-                  <td class="border px-4 py-2">
-                    {{ note.excerpt }}
-                  </td>
-                  <td class="px-4 py-2">
-                    <inertia-link :href="route('notes.show', note.id)"
-                      >Show</inertia-link
-                    >
-                  </td>
-                  <td class="px-4 py-2">
-                    <inertia-link :href="route('notes.edit', note.id)"
-                      >Edit</inertia-link
-                    >
-                  </td>
-                </tr>
-              </table>
+              <p>{{ note.content }}</p>
+              <hr class="my-6" />
+              <inertia-link :href="route('notes.index')"> Back </inertia-link>
             </div>
           </div>
         </div>
@@ -55,7 +42,7 @@ export default {
   },
 
   props: {
-    notes: [],
+    note: {},
   },
 };
 </script>
